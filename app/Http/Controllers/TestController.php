25bbbcc2c7d8540057105d6cd92a3c4f26d280a0
bino <hoangpham23052003@gmail.com>
2024-04-02 09:03:03 +0700

    Fix file boot Create TestController

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\File;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        // TODO
+    }
+}
