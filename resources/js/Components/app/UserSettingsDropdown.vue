f7d6b60bdd32da2aaa09a1225c0dd1b665b472df
bino <hoangpham23052003@gmail.com>
2024-04-02 13:48:54 +0700

    Fix small CSS issues on settings dropdown

--- resources/js/Components/app/UserSettingsDropdown.vue
+++ resources/js/Components/app/UserSettingsDropdown.vue
@@ -7,7 +7,7 @@ import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
 <template>
     <Menu as="div" class="relative block text-left">
         <MenuButton
-            class="flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            class="flex w-full justify-center items-center gap-x-1.5 rounded-md bg-white px-3 py-2 font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
         >
             {{ $page.props.auth.user.name }}
             <svg
